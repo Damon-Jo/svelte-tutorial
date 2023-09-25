4c072f9e77a5fab652bc98b3639c9812e05eb8ef
Damon-Jo <lunaticjky@gmail.com>
2023-09-25 10:57:47 -0700

    implemented the input function

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,40 @@
 <script>
-	let todos = ["get milk", "get eggs", 'get bread']
+	let todos = [];
+	let task = "";
+
+
+	const addTodo = () => {
+		let todo = {
+		task: task,
+		isComplete: false,
+		createdAtm: new Date(),
+	}
+
+		todos = [...todos, todo];
+		task="";
+	}
+
+	// $: console.log(todo);
+	$: console.table(todos);
+
 </script>
 
 <main>
+
+	<input type="text" placeholder="Add a task" bind:value={task}/>
+	<button on:click={addTodo}>Add</button>
 	<ol>
 		{#each todos as todo}
-			<li>{todo}</li>
+			<li class:complete={todo.isComplete}>{todo.task}</li>
 		{/each}
 	</ol>
 </main>
 
 <style>
+	.complete {
+		text-decoration: line-through;
+	}
+
 	/* main {
 		text-align: center;
 		padding: 1em;
